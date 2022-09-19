7a1a1b53539a4ca4c5eebc108a17d67c178bd64f
zach-thompson <zach.thompson@marquette.edu>
2022-09-19 18:10:58 -0500

    Tweaks

--- COSC_2100/src/sample_code/FileWrite.java
+++ COSC_2100/src/sample_code/FileWrite.java
@@ -9,15 +9,16 @@ import java.io.IOException;
  */
 public class FileWrite {
 	public static void main(String[] args) throws IOException {
-		FileWriter fw1 = new FileWriter("info2.txt");
+		FileWriter fw1 = new FileWriter("info2.txt", true);
 		//added true to work in "append" mode
 		PrintWriter pw1 = new PrintWriter(fw1);
 		
-		pw1.print("5th run");
-		pw1.print("Bye ");
+		pw1.println("5th run");
+		pw1.println("Bye ");
 		pw1.println("See you soon!!");
 		
 		pw1.println("Next class on Wednesday!!");
+		pw1.println("prince");
 		
 		pw1.close();
 
