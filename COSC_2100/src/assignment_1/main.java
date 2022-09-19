73eba3d9bfd15b44835432a7320e9a955f6661b5
zach-thompson <zach.thompson@marquette.edu>
2022-09-19 14:34:56 -0500

    Prints file to working tree but not local folder

--- COSC_2100/src/assignment_1/main.java
+++ COSC_2100/src/assignment_1/main.java
@@ -8,31 +8,86 @@ package assignment_1;
 */
 
 import java.util.Scanner;
+import java.io.*;
 
 public class main {
+	public static Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		System.out.println("Welcome to Zach's login simulator\n");
+		FileWriter userData = new FileWriter("userData.txt");
+		
+		switch (getUserType()) { // calls getUserType which returns a 1 or 2
+		case 1:
+			login();
+			break;
+		case 2:
+			newUser();
+			break;
+		}
+		return;
+	}
+	
+	public static int getUserType() {
 		int input = 0;
 		
 		while (!(input == 1 || input == 2)) {
-			System.out.print("Enter 1 for existing user - Enter 2 to create a new user: ");
+			System.out.print("Enter 1 to log in as an existing user - Enter 2 to create a new user: ");
 			if (!scan.hasNextInt()) {
-				System.out.println("Invalid response\n");
+				System.out.println("Invalid response, please try again.\n");
 				scan.nextLine();
 			}
 			else {
 				input = scan.nextInt();
 				if (!(input == 1 | input == 2)) {
-					System.out.println("Invalid response\n");
+					System.out.println("Please enter one of the provided options.\n");
 				}
 			}
 		}
+		return input;
+	}
+	
+	public static void login() {
+		// Check for existing user write to file
+		System.out.println("Username: ");
+		String username = scan.nextLine();
 		
-		System.out.println("You entered: " + input);
 		
+		
+		System.out.println("Password: ");
+		String password = scan.nextLine();
+		
+		System.out.println("Login successful!");
 		scan.close();
 		return;
 	}
 
+	public static void newUser() throws IOException {
+		File userData = new File("userData.txt");
+		PrintWriter print = new PrintWriter(userData);
+		Scanner read = new Scanner(userData);
+		
+		System.out.println("Enter a username: ");
+		String username = scan.next();
+		
+		//do {
+			//if (read.nextLine() == username) {
+			//	System.out.println("Username already exists, please try again");
+			//}
+			//else {
+				print.print(username);
+			//}
+	//	} while (read.hasNextLine());
+		
+		//System.out.println("Enter a username: ");
+		//String password = scan.nextLine();
+		
+		
+		
+		System.out.println("New user created!");
+		scan.close();
+		print.close();
+		read.close();
+		return;
+	}
 }
